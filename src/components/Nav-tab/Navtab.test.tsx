0e768aabec6816f0adb5afb10cbe196ffff702b7
Angel Afube <angel@zestpayment.com>
2024-07-31 13:14:11 +0100

    modify navtab test

--- src/components/Nav-tab/Navtab.test.tsx
+++ src/components/Nav-tab/Navtab.test.tsx
@@ -1,42 +1,18 @@
-// NavTabs.test.js
-import { fireEvent, render, screen } from "@testing-library/react";
-
-import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 
 import NavTabs from "./Navtab";
 
-test("renders NavTabs component with tabs", () => {
-  render(<NavTabs />);
+import "@testing-library/jest-dom";
 
-  // Check if all tabs are rendered
-  expect(screen.getByText("Profile")).toBeInTheDocument();
-  expect(screen.getByText("Language")).toBeInTheDocument();
-  expect(screen.getByText("Notifications")).toBeInTheDocument();
-  expect(screen.getByText("Invite Link")).toBeInTheDocument();
-  expect(screen.getByText("Security")).toBeInTheDocument();
-});
-
-test("clicking on a tab updates the selected tab", () => {
-  render(<NavTabs />);
-
-  // Click on the 'Language' tab
-  fireEvent.click(screen.getByText("Language"));
-
-  // Check if 'Language' tab is selected
-  expect(screen.getByText("Language")).toHaveClass(
-    "bg-secondary-110 text-white border-black",
-  );
-  // Check if other tabs are not selected
-  expect(screen.getByText("Profile")).not.toHaveClass(
-    "bg-secondary-110 text-white border-black",
-  );
-  expect(screen.getByText("Notifications")).not.toHaveClass(
-    "bg-secondary-110 text-white border-black",
-  );
-  expect(screen.getByText("Invite Link")).not.toHaveClass(
-    "bg-secondary-110 text-white border-black",
-  );
-  expect(screen.getByText("Security")).not.toHaveClass(
-    "bg-secondary-110 text-white border-black",
-  );
+describe("NavTabs", () => {
+  it("renders desktop logo", () => {
+    expect.assertions(5);
+    render(<NavTabs />);
+    expect(screen.getByText("Profile")).toBeInTheDocument();
+    expect(screen.getByText("Language")).toBeInTheDocument();
+    expect(screen.getByText("Notifications")).toBeInTheDocument();
+    expect(screen.getByText("Invite Link")).toBeInTheDocument();
+    expect(screen.getByText("Security")).toBeInTheDocument();
+  });
 });
