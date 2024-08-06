f5c6acf9af3fd9fcf2c347c995158bf1be677c20
Angel Afube <angel@zestpayment.com>
2024-08-06 13:06:50 +0100

    add step component

--- src/app/how-it-works/page.tsx
+++ src/app/how-it-works/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import DarkNav from "~/components/DarkNav/DarkNav";
 import HeroSection from "~/components/HowItWorks/Hero/Hero";
+import { StepTabs } from "~/components/HowItWorks/Steps/Steps";
 import LightNav from "~/components/light-navbar/LightNav";
 
 export default function HowItWorks() {
@@ -9,6 +10,7 @@ export default function HowItWorks() {
     <>
       <LightNav />
       <HeroSection />
+      <StepTabs />
     </>
   );
 }
