b434a2b8555fcdf0fe0804c59552c5f7b9f54119
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2024-08-06 15:12:25 +0100

    fix

--- src/app/guides/page.tsx
+++ src/app/guides/page.tsx
@@ -10,7 +10,6 @@ import LanguageSelector from "~/components/common/dropdowns/LanguageSelector";
 import SearchBar from "~/components/common/searchbar/Searchbar";
 import SearchIcon from "~/components/common/searchbar/SearchIcon";
 import Correct from "~/components/correct";
-import DarkNav from "~/components/DarkNav/DarkNav";
 import ButtonGrid from "~/components/gameNavigationButtons/ButtonGrid";
 import LevelDisplay from "~/components/level-display/LevelDisplay";
 import MicrophoneComponent from "~/components/microphone/MicrophoneComponent";
