9f8daa36b31b03ea69d14d85750532a27254c82f
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2024-08-06 16:24:24 +0100

    build fix

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,57 +1,76 @@
-"use client"
-import React, { useState, useRef } from 'react';
+"use client";
+
+import Image from "next/image";
+import React, { useRef, useState } from "react";
 
 const backgrounds = [
-  { video: '/images/landing/firebgvideo8sec.mp4', thumbnail: '/images/landing/HeroBgHouseONFire.jpeg' },
-  { video: '/images/landing/backgroundVideo1.mp4', thumbnail: '/images/landing/HeroInitBgImg.png' },
-  { video: '/images/landing/backgroundVideo1.mp4', thumbnail: '/images/landing/HeroBgBedRoom.jpeg' },
-  { video: '/images/landing/backgroundVideo1.mp4', thumbnail: '/images/landing/HeroBgAccident.jpeg' },
+  {
+    video: "/images/landing/firebgvideo8sec.mp4",
+    thumbnail: "/images/landing/HeroBgHouseONFire.jpeg",
+  },
+  {
+    video: "/images/landing/backgroundVideo1.mp4",
+    thumbnail: "/images/landing/HeroInitBgImg.png",
+  },
+  {
+    video: "/images/landing/backgroundVideo1.mp4",
+    thumbnail: "/images/landing/HeroBgBedRoom.jpeg",
+  },
+  {
+    video: "/images/landing/backgroundVideo1.mp4",
+    thumbnail: "/images/landing/HeroBgAccident.jpeg",
+  },
 ];
-
 
 const HeroSection: React.FC = () => {
   const [activeBgIndex, setActiveBgIndex] = useState<number>(0);
-  const videoRef = useRef<HTMLVideoElement>(null)
+  const videoReference = useRef<HTMLVideoElement>(null);
 
   const activeBg = backgrounds[activeBgIndex];
 
   return (
-    <section className="flex h-screen w-full flex-col p-4 mx-auto items-center justify-center">
-      <div className="relative flex h-full w-full flex-col items-center justify-center bg-cover bg-center bg-no-repeat overflow-hidden transition-all duration-500 ease-in-out">
+    <section className="mx-auto flex h-screen w-full flex-col items-center justify-center p-4">
+      <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden bg-cover bg-center bg-no-repeat transition-all duration-500 ease-in-out">
         <video
-          ref={videoRef}
-          className="absolute top-0 left-0 w-full h-full object-cover object-bottom"
+          ref={videoReference}
+          className="absolute left-0 top-0 h-full w-full object-cover object-bottom"
           src={activeBg.video}
           autoPlay
           muted
           loop
           poster={activeBg.thumbnail}
         />
-        <div className="absolute top-0 left-0 right-0 h-[60px] bg-gradient-to-b from-secondary-100 via-transparent-black-10 opacity-85 to-transparent"></div>
+        <div className="absolute left-0 right-0 top-0 h-[60px] bg-gradient-to-b from-secondary-100 via-transparent-black-10 to-transparent opacity-85"></div>
 
-        <h1 className={`relative z-10 font-axiformaMedium text-center text-4xl sm:text-4xl md:text-5xl lg:text-[62px] font-semibold leading-tight sm:leading-snug md:leading-normal lg:leading-[90px] tracking-wide sm:w-full w-[370px] max-w-[400px] sm:max-w-[630px] text-white`}>
+        <h1
+          className={`relative z-10 w-[370px] max-w-[400px] text-center font-axiformaMedium text-4xl font-semibold leading-tight tracking-wide text-white sm:w-full sm:max-w-[630px] sm:text-4xl sm:leading-snug md:text-5xl md:leading-normal lg:text-[62px] lg:leading-[90px]`}
+        >
           Learn Languages The Fun Way
         </h1>
 
-        <div className="absolute bottom-0 flex flex-col items-center justify-center border-[#FFFFFF20] border rounded-t-[36px] bg-[#212121D9] w-[90%] sm:w-[80%] md:w-[70%] lg:w-[408px]">
-          <div className="flex flex-row w-full justify-center items-center space-x-2 sm:space-x-3 md:space-x-4 lg:space-x-5 px-2 sm:px-3 md:px-4 lg:px-5 pt-5 pb-3">
+        <div className="absolute bottom-0 flex w-[90%] flex-col items-center justify-center rounded-t-[36px] border border-[#FFFFFF20] bg-[#212121D9] sm:w-[80%] md:w-[70%] lg:w-[408px]">
+          <div className="flex w-full flex-row items-center justify-center space-x-2 px-2 pb-3 pt-5 sm:space-x-3 sm:px-3 md:space-x-4 md:px-4 lg:space-x-5 lg:px-5">
             {backgrounds.map((bg, index) => (
               <div
                 key={index}
-                className={`w-[18%] sm:w-[20%] md:w-[22%] lg:w-[90px] aspect-square bg-cover bg-center rounded-2xl cursor-pointer transition-all duration-300 ${activeBgIndex === index ? 'border-2 border-white' : 'border border-transparent'}`}
+                className={`aspect-square w-[18%] cursor-pointer rounded-2xl bg-cover bg-center transition-all duration-300 sm:w-[20%] md:w-[22%] lg:w-[90px] ${activeBgIndex === index ? "border-2 border-white" : "border border-transparent"}`}
                 onClick={() => setActiveBgIndex(index)}
               >
-                <img
-                  className="w-full h-full object-cover object-center rounded-2xl"
+                <Image
+                  className="h-full w-full rounded-2xl object-cover object-center"
                   src={bg.thumbnail}
                   alt={`Thumbnail ${index + 1}`}
+                  width={300}
+                  height={300}
                 />
               </div>
             ))}
           </div>
 
-          <div className="flex flex-row justify-center items-center w-full border-t border-[#FFFFFF40] py-2 sm:py-3 md:py-4">
-            <h3 className="text-white text-sm sm:text-base md:text-lg lg:text-[18px] font-axiformaMedium">Explore Quests</h3>
+          <div className="flex w-full flex-row items-center justify-center border-t border-[#FFFFFF40] py-2 sm:py-3 md:py-4">
+            <h3 className="font-axiformaMedium text-sm text-white sm:text-base md:text-lg lg:text-[18px]">
+              Explore Quests
+            </h3>
           </div>
         </div>
       </div>
@@ -60,8 +79,5 @@ const HeroSection: React.FC = () => {
 };
 
 export default HeroSection;
-
-
-
 
 // ${ activeBgIndex === 2 ? 'text-black' : 'text-white' }
--- src/components/LandingPageComponents/HomeSection4.tsx
+++ src/components/LandingPageComponents/HomeSection4.tsx
@@ -1,20 +1,24 @@
-import React from 'react'
-
 const HomeSection4 = () => {
-    return (
-        <div className='bg-secondary-100 w-full p-4 sm:p-6 md:p-10'>
-            <div className='bg-white w-full px-4 py-16 sm:px-6 sm:py-12 md:px-8 md:py-16 lg:px-20 lg:py-32'>
-                <div className='flex flex-col lg:flex-row justify-center items-center gap-6 lg:gap-14 max-w-7xl mx-auto'>
-                    <h1 className='text-secondary-60 text-2xl sm:text-3xl md:text-4xl font-axiformaSemiBold font-semibold leading-tight tracking-wide text-center lg:text-left lg:w-1/2'>
-                        Unlock the Power of <b className='text-secondary-120'>Language Learning</b> with Our <b className='text-secondary-120'>Innovative</b> and <b className='text-secondary-120'>Engaging Features</b>
-                    </h1>
-                    <p className='font-axiformaMedium max-w-[312px] md:max-w-[700px] font-normal text-sm sm:text-base leading-relaxed text-center lg:text-left lg:w-1/2'>
-                        Delve into various real-life scenarios to be prepared for anything and everything. Master pronunciation with our AI accuracy tool, converse with characters in interactive scenes, expand your vocabulary, and set your difficulty level from beginner to expert. 
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="w-full bg-secondary-100 p-4 sm:p-6 md:p-10">
+      <div className="w-full bg-white px-4 py-16 sm:px-6 sm:py-12 md:px-8 md:py-16 lg:px-20 lg:py-32">
+        <div className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-6 lg:flex-row lg:gap-14">
+          <h1 className="text-center font-axiformaSemiBold text-2xl font-semibold leading-tight tracking-wide text-secondary-60 sm:text-3xl md:text-4xl lg:w-1/2 lg:text-left">
+            Unlock the Power of{" "}
+            <b className="text-secondary-120">Language Learning</b> with Our{" "}
+            <b className="text-secondary-120">Innovative</b> and{" "}
+            <b className="text-secondary-120">Engaging Features</b>
+          </h1>
+          <p className="max-w-[312px] text-center font-axiformaMedium text-sm font-normal leading-relaxed sm:text-base md:max-w-[700px] lg:w-1/2 lg:text-left">
+            Delve into various real-life scenarios to be prepared for anything
+            and everything. Master pronunciation with our AI accuracy tool,
+            converse with characters in interactive scenes, expand your
+            vocabulary, and set your difficulty level from beginner to expert.
+          </p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default HomeSection4
+export default HomeSection4;
