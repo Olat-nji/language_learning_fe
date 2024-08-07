b3782bff06970066bfc23bd5295172bdc2f12af0
= <=>
2024-08-07 17:37:29 +0100

    fix/LLAF: eslint issues

--- src/components/AdminNav/NavGradientAnimation.tsx
+++ src/components/AdminNav/NavGradientAnimation.tsx
@@ -1,5 +1,5 @@
 export default function NavGradientAnimation() {
   return (
-    <div className="animate-gradient-border bg-nav-gradient bottom-[-20px] left-0 h-[12px] w-full bg-[length:200%_200%]"></div>
+    <div className="bottom-[-20px] left-0 h-[12px] animate-gradient-border bg-nav-gradient w-full bg-[length:200%_200%]"></div>
   );
 }
