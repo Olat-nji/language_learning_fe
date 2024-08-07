f7c809fe2160712ba77fb4592e4517478d43b195
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-08-07 15:10:16 +0100

    feat: language selector test

--- src/test/LanguageSelector.test.tsx
+++ src/test/LanguageSelector.test.tsx
@@ -1,32 +1,23 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-
 import "@testing-library/jest-dom";
-
 import LanguageSelector from "~/components/common/dropdowns/LanguageSelector";
 
-describe("languageSelector", () => {
-  it("renders Language button", () => {
-    expect.assertions(1);
+describe("LanguageSelector", () => {
+  it("renders without crashing", () => {
     render(<LanguageSelector />);
-    expect(screen.getByText("Language")).toBeInTheDocument();
+    expect(screen.getByRole("button")).toBeInTheDocument();
   });
 
   it("toggles dropdown on button click", () => {
-    expect.assertions(2);
     render(<LanguageSelector />);
-    const button = screen.getByText("Language");
-    fireEvent.click(button);
-    expect(screen.getByText("French")).toBeInTheDocument();
-    fireEvent.click(button);
-    expect(screen.queryByText("French")).not.toBeInTheDocument();
-  });
-
-  it("renders languages in dropdown", () => {
-    expect.assertions(2);
-    render(<LanguageSelector />);
-    const button = screen.getByText("Language");
-    fireEvent.click(button);
+    const button = screen.getByRole("button");
+    
+    fireEvent.click(button); 
     expect(screen.getByText("French")).toBeInTheDocument();
     expect(screen.getByText("Select new language")).toBeInTheDocument();
+
+    fireEvent.click(button); 
+    expect(screen.queryByText("French")).not.toBeInTheDocument();
+    expect(screen.queryByText("Select new language")).not.toBeInTheDocument();
   });
 });
