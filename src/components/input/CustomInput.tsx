467e2ff8a825ee8c35c96c6231569f590adb4db0
= <=>
2024-08-07 15:01:34 +0100

    fix/(LLAG-112): fix eslint issues

--- src/components/input/CustomInput.tsx
+++ src/components/input/CustomInput.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import React, { ChangeEvent, ChangeEventHandler, useState } from "react";
+import React, { ChangeEventHandler } from "react";
 
 type Properties = {
   placeholder?: string;
   Icon?: string | React.ElementType;
-  onChange?: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement> | undefined;
+  onChange?:
+    | ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>
+    | undefined;
   error?: string;
   label?: string;
   inputType?: "password" | "text" | "textarea";
@@ -36,23 +38,24 @@ const CustomInput: React.FC<Properties> = ({
         >
           {Icon && <Icon data-testid="custom-icon" className="text-grey-500" />}
 
-          {inputType === "textarea" ? 
-          <textarea
-            name={name}
-            value={value}
-            placeholder={placeholder}
-            onChange={onChange}
-            className={`${className} placeholder:text-seconday-40 min-h-[100px] flex-1 text-[14px] outline-none`}
-          />
-          :
-          <input
-            name={name}
-            type={inputType}
-            value={value}
-            placeholder={placeholder}
-            onChange={onChange}
-            className={`${className} placeholder:text-seconday-40 h-[18px] flex-1 text-[14px] outline-none`}
-          />}
+          {inputType === "textarea" ? (
+            <textarea
+              name={name}
+              value={value}
+              placeholder={placeholder}
+              onChange={onChange}
+              className={`${className} placeholder:text-seconday-40 min-h-[100px] flex-1 text-[14px] outline-none`}
+            />
+          ) : (
+            <input
+              name={name}
+              type={inputType}
+              value={value}
+              placeholder={placeholder}
+              onChange={onChange}
+              className={`${className} placeholder:text-seconday-40 h-[18px] flex-1 text-[14px] outline-none`}
+            />
+          )}
 
           <p className="text-body-s text-red mt-[16px]">{error}</p>
         </div>
