846c3d5d79014175c7a5d97ed05857900d64c2c5
Jibola Paul <harjibbolar26@gmail.com>
2024-08-07 18:32:39 +0100

    fix: fix error

--- src/app/dashboard/(user-dashboard)/profile-settings/layout.tsx
+++ src/app/dashboard/(user-dashboard)/profile-settings/layout.tsx
@@ -12,7 +12,7 @@ export default function SettingsLayout({
       <header className="flex w-full max-w-[900px] rounded-[0px] border border-neutral-40 bg-white px-4 py-4 sm:rounded-[10px]">
         <p className="text-[20px] sm:text-[30px]">Account Settings</p>
       </header>
-      <div className="mt-0 flex w-full gap-[40px] sm:mt-[36px] sm:max-w-[900px] md:flex-row">
+      <div className="mt-4 flex w-full gap-[40px] sm:mt-[36px] sm:max-w-[900px] md:flex-row">
         <ProfileSettingsMenu />
         <div className="flex w-full grow flex-col gap-[48px] rounded-[18px] max-md:mr-6">
           <Suspense>{children}</Suspense>
